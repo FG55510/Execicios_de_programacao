7d2087c3310ff3a760408781b60debbe1f1bf174
Fernanda5510 <130413112+Fernanda5510@users.noreply.github.com>
2023-04-10 22:20:13 -0300

    Add files via upload

---
+++ Progama_Variaveis(Megaman).cpp
@@ -0,0 +1,23 @@
+#include <iostream> //biblioteca que permite input do usuario
+using namespace std;
+
+int main() {
+    
+    int hp;
+    float energy;
+    bool status;
+    string charactername;    //criacao da variaveis de quantidade de vida e energia, status e o nome do personagem
+    
+    hp = 10;
+    energy = 50.00;
+    status = true;
+    charactername = "Zero";
+    //Atribuicao de nomes a estes personagens
+
+    
+
+    cout << hp <<endl;
+    cout << energy <<endl;
+    cout << status <<endl;
+    cout << charactername <<endl;
+    //Motrar as informacoes para o usuario
