a513e91886eca4cb45a3c55bd837b0455a09afc8
Fernanda5510 <130413112+Fernanda5510@users.noreply.github.com>
2023-04-10 21:02:28 -0300

    Add files via upload

--- Circulo.cpp
+++ Circulo.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main() {
-    float raio, area;
-    const float Pi = 3.14159;
-    cout << "Digite o raio do circulo: ";
-    cin >> raio;
+    float raio, area; //variaveis float que amarzenam o valor da area e do raio
+    const float Pi = 3.14159; //valor constante de pi armazenado
+    cout << "Digite o raio do circulo: "; // interface para instrucao do usuario
+    cin >> raio; //entrada do valor do raio pelo usuario
     
-    area = Pi * raio * raio;
-    cout << "A area do circulo eh " << area << endl;
+    area = Pi * raio * raio; //calculo da area
+    cout << "A area do circulo eh " << area << endl; //interface do usuario mostando o valor da area
 }
